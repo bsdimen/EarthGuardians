1adbd414dbe7ae54ba3c8a99b91d18d1bc49fa03
bsdimen <nimene505@gmail.com>
2024-05-28 18:20:50 +0100

    C-28/05/2024-ADDED ressource.js -MODIFIED educational-js

--- src/Pages/educational.js
+++ src/Pages/educational.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import  { useState, useEffect } from 'react';
-import article_cover_item from "../IMGS/articles-covers/article-cover-01.jpg";
+import Ressource from './ressource.js';
 
 const Educational = () => {
   const [data, setData] = useState([]);
-  const [type, setType] = useState('articles'); // Set default type
+  const [selectedSection, setSelectedSection] = useState('articles');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,94 +22,31 @@ const Educational = () => {
     fetchData();
   }, []);
 
-  switch (type) {
-    case 'articles':
-      return (
-        <div className="edu-content">
-          <div className="articles">
-            <div className="articles-list">
-              {data && data.map(resource => {
-                if (resource.type === "article") {
-                  return (
-                    <div key={resource.id} className="article-item">
-                      <div className="article-item-cover">
-                        <img src={article_cover_item} alt="Article cover" className="article-item-cover-img" />
-                      </div>
-                      <div className="article-item-content">
-                        <h3>{resource.title}</h3>
-                        <p>{resource.publication_date}</p>
-                        <p>{resource.description}</p>
-                        <p>Written by <span>{resource.author}</span></p>
-                      </div>
-                    </div>
-                  );
-                } else {
-                  return null; // return null if the type is not "article"
-                }
-              })}
-            </div>
-          </div>
+  const handleSectionClick = (section) => {
+    setSelectedSection(section);
+  };
+
+  const getClassNames = (section) => {
+    return selectedSection === section ? 'active' : '';
+  };
+
+  return (
+    <div className="educational">
+      <div className='edu-content'>
+        <h1>Educational Resources</h1>
+        <p>Explore our library of articles, videos, and interactive tools to learn more about environmental protection, climate science, and sustainable living practices. Whether you're a student, educator, or concerned citizen, our resources are designed to inform, inspire, and empower you to make a difference in the world.</p>
+      </div>
+      <div className={`edu-container ${getClassNames(selectedSection)}`}>
+        <div className='edu-links'>
+          <button onClick={() => handleSectionClick('article')} className= {"edu-link "+ getClassNames('article')}>Articles</button>
+          <button onClick={() => handleSectionClick('book')} className={"edu-link "+ getClassNames('book')}>Books</button>
+          <button onClick={() => handleSectionClick('documentary')} className={"edu-link "+ getClassNames('documentary')}>Documentaries</button>
         </div>
-      );
-    case 'books':
-      return (
-        <div className="edu-content">
-          <div className="books">
-            <div className="books-list">
-              {data && data.map(resource => {
-                if (resource.type === "book") {
-                  return (
-                    <div key={resource.id} className="book-item">
-                      <div className="book-item-cover">
-                        <img src={resource.image} alt="Book cover" className="book-item-cover-img" />
-                      </div>
-                      <div className="book-item-content">
-                        <h3>{resource.title}</h3>
-                        <p>{resource.publication_date}</p>
-                        <p>{resource.description}</p>
-                        <p>Written by <span>{resource.author}</span></p>
-                      </div>
-                    </div>
-                  );
-                } else {
-                  return null; // return null if the type is not "book"
-                }
-              })}
-            </div>
-          </div>
-        </div>
-      );
-    case 'documentaries':
-      return (
-        <div className="edu-content">
-          <div className="documentaries">
-            <div className="documentaries-list">
-              {data && data.map(resource => {
-                if (resource.type === "documentary") {
-                  return (
-                    <div key={resource.id} className="documentary-item">
-                      <div className="documentary-item-cover">
-                        <img src={resource.image} alt="Documentary cover" className="documentary-item-cover-img" />
-                      </div>
-                      <div className="documentary-item-content">
-                        <h3>{resource.title}</h3>
-                        <p>{resource.release_date}</p>
-                        <p>{resource.description}</p>
-                        <p>Directed by <span>{resource.director}</span></p>
-                      </div>
-                    </div>
-                  );
-                } else {
-                  return null; // return null if the type is not "documentary"
-                }
-              })}
-            </div>
-          </div>
-        </div>
-      );
-    default:
-      return null; // handle other types or return null
-  }
-};
+    </div>
+    <Ressource path={selectedSection}  data={data} />
+    </div>
+  )
+ } 
+
 
 export default Educational;

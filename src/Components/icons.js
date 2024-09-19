c4f2b31bc8f9afbdb963f77d7e5bb6bc147814fa
bsdimen <nimene505@gmail.com>
2024-09-19 20:37:45 +0100

    19-09

---
+++ src/Components/icons.js
@@ -0,0 +1,16 @@
+export const ArrowRightIcon = () => {
+    return <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M14.43 5.93005L20.5 12.0001L14.43 18.0701" stroke="#171717" stroke-width="2" stroke-miterlimit="10" />
+        <path d="M3.5 12H20.33" stroke="#171717" stroke-width="2" stroke-miterlimit="10" />
+    </svg>
+
+}
+export const ArrowLeftIcon = () => {
+    return <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M9.57 5.93005L3.5 12.0001L9.57 18.0701" stroke="#171717" stroke-width="2" stroke-miterlimit="10" />
+        <path d="M20.5 12H3.67" stroke="#171717" stroke-width="2" stroke-miterlimit="10" />
+    </svg>
+
+}
+
+
--- src/Pages/blog.js
+++ src/Pages/blog.js
@@ -2,9 +2,11 @@ import Header from "../Components/header";
 import { motion } from "framer-motion";
 import { AnimatedHeading } from "../Components/prefabs"
 import { useRef, useState, useEffect } from "react";
-
+import { ArrowLeftIcon, ArrowRightIcon } from "../Components/icons";
 
 export default function Blog() {
+
+
     return (
         <div className="Blog">
             <div className="center-h">
@@ -32,11 +34,29 @@ export default function Blog() {
 
                             </div>
                         </div>
+                    </div>
 
+                    <div className="latest-posts-section center-h center-v">
+                        <div className="container">
+                            <div className="latest-post-carsoul">
+                                <div className="browse-articles">
+                                    <h4>See our more articles and browse</h4>
+                                    <button className="btn-primary" data-content="Browse more" >browse more</button>
+                                </div>
+                                <div className="posts-grid">
+                                    <Post />
+                                    <Post />
+                                    <Post />
+                                    <Post />
+                                    <Post />
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
 
     )
 }
@@ -44,40 +64,86 @@ export default function Blog() {
 const Category = ({ title, parag }) => {
 
     const [isItemHovering, setIsItemHovering] = useState(false)
+    const [IsBtnHovering, setIsBtnHovering] = useState(false)
+
     const contentRef = useRef();
-    const parentRef = useRef();
 
-
-    let top;
+    const [top, setTop] = useState();
 
     useEffect(() => {
         const contentheight = contentRef.current.offsetHeight; // or getBoundingClientRect().height
         console.log(`cHeight: ${contentheight}px`);
+        setTop(-contentheight + 20);
 
-        const parentheight = parentRef.current.offsetHeight; // or getBoundingClientRect().height
-        console.log(`pHeight: ${parentheight}px`);
-
-        top = - (100 - ((contentheight * 100) / parentheight));
         console.log(`top: ${top}`);
-    }, [contentRef, parentRef]);
+    }, [contentRef]);
 
     return <motion.div
         onMouseEnter={() => setIsItemHovering(true)}
         onMouseLeave={() => setIsItemHovering(false)}
-        ref={parentRef}
         className="category-item">
         <div className="category-info"
             ref={contentRef}>
             <h6>{title}</h6>
-            <p>{parag}</p>
+            {/* <p>{parag}</p> */}
         </div>
         <motion.div
             className="category-hover"
             initial={{ y: "100%" }}
-            animate={isItemHovering ? { y: { top } } : { y: "100%" }}
+            animate={isItemHovering ? { y: `${top}px` } : { y: "100%" }}
             transition={{ type: "tween", duration: 0.3 }} // Optional: Adjust the spring effect
         >
-            <button className="btn-shadow">See More</button>
+            <motion.button
+                onMouseEnter={() => setIsBtnHovering(true)}
+                onMouseLeave={() => setIsBtnHovering(false)}
+                animate={IsBtnHovering ? { color: "#4E8905", borderColor: "#4E8905" } : { color: "#F5F5F5", borderColor: "#F5F5F5" }}
+                className="btn-shadow whtite relative">
+                See More
+                <motion.span
+                    initial={{ width: "0", height: "0" }}
+                    animate={IsBtnHovering ? { width: "100%", height: "100%" } : { width: "0", height: "0" }}
+                    transition={{ duration: 0.2 }}
+                    className="btn-animation"></motion.span>
+            </motion.button>
         </motion.div>
     </motion.div >
+}
+
+const Post = () => {
+    return <div className="single-post">
+        <div className="single-post-content">
+            <h6>The Power of Collective Action: How Communities Can Save the Planet</h6>
+            <p>Posted on September 15, 2024</p>
+        </div>
+    </div>
+}
+
+const Icons = () => {
+    const [hoverRightIcon, setHoverRightIcon] = useState(false);
+    const [hoverLeftIcon, setHoverLeftIcon] = useState(false);
+
+    return <div className="icons">
+        <motion.button
+            onMouseEnter={() => setHoverLeftIcon(true)}
+            onMouseLeave={() => setHoverLeftIcon(false)}
+            className="left">
+            <ArrowLeftIcon />
+            <motion.span
+                initial={{ width: "0", height: "0" }}
+                animate={hoverLeftIcon ? { width: "100%", height: "100%" } : { width: "0", height: "0" }}
+                transition={{ duration: 0.2 }}
+            ></motion.span>
+        </motion.button>
+        <motion.button
+            onMouseEnter={() => setHoverRightIcon(true)}
+            onMouseLeave={() => setHoverRightIcon(false)}
+            className="right">
+            <ArrowRightIcon />
+            <motion.span
+                initial={{ width: "0", height: "0" }}
+                animate={hoverRightIcon ? { width: "150%", height: "150%" } : { width: "0", height: "0" }}
+                transition={{ duration: 0.2 }}
+            ></motion.span>
+        </motion.button>
+    </div>
 }
